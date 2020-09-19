e4dc67782125bd9c7c4c784c73703a3c961ede61
Ssegura09 <stephaniemsegura09@gmail.com>
2020-09-19 00:00:42 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    def about
+        
+    end
+
+end
